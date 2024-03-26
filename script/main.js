84e1be7bd4f4f07e3dd34184878b6c4ac086594d
rugsmunny <karim.sendesni@gmail.com>
2024-03-26 13:05:04 +0100

    animation added, transitions

--- script/main.js
+++ script/main.js
@@ -9,32 +9,50 @@ function sayHi(){
     alarmBtn.onclick = playback;
     document.querySelector('#test').textContent = "SCRIPTET FUNKAR!";
     document.querySelector('body').append(alarmBtn);
-}*/
 
-function playback(event){
-    event.preventDefault();
-    const soundToGet = '8000_50';
+        const soundToGet = '8000_50';
     console.log(soundToGet);
     new Audio(`resources/sounds/testsounds/${soundToGet}.ogg`).play();
+}*/
+const modal = document.querySelector('.app');
+let testStarted = false;
+let timeoutID = null;
+function startTest(event){
+    event.preventDefault();
+
+    testStarted = true;
+    clearTimeout(timeoutID);
+    const infoSegments = document.querySelectorAll('.info');
+    infoSegments.forEach(segment => segment.classList.add('hide'));
 }
 
 const startBtn = document.querySelector('#start-test-btn');
 
-startBtn.onclick = playback;
+startBtn.onclick = startTest;
+
 
 function changeBackgroundPictures() {
-    const modal = document.querySelector('.modal');
+    
     const imagesFolder = 'resources/images/hearing_buds/';
     const imageNames = ['1-black-box-top.png','2-closed-black-box-top-45-degree.png','3-closed-black-box-front.png','4-open-case-front-no-grip.png', '5-closer-to-tilted-case.png', '6-up-down-no-grip.png', '7-big-small-no-grip.png','8-all-tips-no-grip.png','9-phone-with-aqua-cover-open.png'];
     let currentIndex = 0;
-
+    console.log(`changeBackgroundPictures current index ${currentIndex}`);
     function changeImage() {
+        console.log(`changeImage current index ${currentIndex}`);
         const imageUrl = `url('${imagesFolder}${imageNames[currentIndex]}')`;
         modal.style.backgroundImage = `${imageUrl}, radial-gradient(circle at 0%, #008545 20%, #008545 55%, #fff 55%)`;
         currentIndex = (currentIndex + 1) % imageNames.length;
-        setTimeout(changeImage, 3500); 
+        timeoutID  = setTimeout(changeImage, 3500); 
+        console.log(`changeBIMMAGE current index ${currentIndex}`);
+        if(testStarted) {
+            console.log('testStarted: ', testStarted);
+            return;
+        }
     }
-
+    if(testStarted) {
+        console.log('testStarted: ', testStarted);
+        return;
+    }
     changeImage();
 }
 
