b3a43b6b3a8916ef1d0df55d56bf7f0d01910e60
rugsmunny <karim.sendesni@gmail.com>
2024-03-26 10:55:22 +0100

    updates on intropage, iterating hearingbuds images, set design correlating to Doro web page

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
-/*
-document.addEventListener('DOMContentLoaded', setTimeout(sayHi, 3000));
 
+
+
+document.addEventListener('DOMContentLoaded', changeBackgroundPictures);
+/*
 function sayHi(){
     const alarmBtn = Object.assign(document.createElement('button'), { id: 'alarm'});
     alarmBtn.innerText = 'PUSH ME';
@@ -19,3 +21,23 @@ function playback(event){
 const startBtn = document.querySelector('#start-test-btn');
 
 startBtn.onclick = playback;
+
+
+
+
+function changeBackgroundPictures() {
+    const modal = document.querySelector('.modal');
+    const imagesFolder = 'resources/images/hearing_buds/';
+    const imageNames = ['1-black-box-top.png','2-closed-black-box-top-45-degree.png','3-closed-black-box-front.png','4-open-case-front-no-grip.png', '5-closer-to-tilted-case.png', '6-up-down-no-grip.png', '7-big-small-no-grip.png','8-all-tips-no-grip.png','9-phone-with-aqua-cover-open.png'];
+    let currentIndex = 0;
+
+    function changeImage() {
+        const imageUrl = `url('${imagesFolder}${imageNames[currentIndex]}')`;
+        modal.style.backgroundImage = `${imageUrl}, radial-gradient(circle at 0%, #008545 20%, #008545 55%, #fff 55%)`;
+        currentIndex = (currentIndex + 1) % imageNames.length;
+        setTimeout(changeImage, 3500); 
+    }
+
+    changeImage();
+}
+
