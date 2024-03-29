195038d171fd8a69edc42ac6de09be84783eda5f
rugsmunny <karim.sendesni@gmail.com>
2024-03-29 13:12:56 +0100

    sync latest updates

--- script/main.js
+++ script/main.js
@@ -15,46 +15,49 @@ function sayHi(){
     new Audio(`resources/sounds/testsounds/${soundToGet}.ogg`).play();
 }*/
 const modal = document.querySelector('.app');
-const form = document.querySelector('.form');
+const form = document.querySelector('.title');
 let testStarted = false;
 let timeoutID = null;
-function startTest(event){
+function startTest(event) {
     event.preventDefault();
     testStarted = true;
-    clearTimeout(timeoutID);
     const infoSegments = document.querySelectorAll('.info');
     infoSegments.forEach(segment => segment.classList.add('hide'));
     modal.classList.add('hide');
-    form.innerHTML = `<p class="miffo">Testet har startat. Du kommer att höra en serie ljud. Klicka på knappen när du hör ljudet.</p>`;
+    form.textContent = `Testet har startat. Du kommer att höra en serie ljud. Klicka på knappen när du hör ljudet.`;
 }
 
 const startBtn = document.querySelector('#start-test-btn');
+
 
 startBtn.onclick = startTest;
 let imageUrl = '';
 
 function changeBackgroundPictures() {
-    
     const imagesFolder = 'resources/images/hearing_buds/';
-    const imageNames = ['1-black-box-top.png','2-closed-black-box-top-45-degree.png','3-closed-black-box-front.png','4-open-case-front-no-grip.png', '5-closer-to-tilted-case.png', '6-up-down-no-grip.png', '7-big-small-no-grip.png','8-all-tips-no-grip.png','9-phone-with-aqua-cover-open.png'];
+    const imageNames = ['1-black-box-top.png', '2-closed-black-box-top-45-degree.png', '3-closed-black-box-front.png', '4-open-case-front-no-grip.png', '5-closer-to-tilted-case.png', '6-up-down-no-grip.png', '7-big-small-no-grip.png', '8-all-tips-no-grip.png', '9-phone-with-aqua-cover-open.png'];
     let currentIndex = 0;
-    console.log(`changeBackgroundPictures current index ${currentIndex}`);
+    let currentImage = 0;
+    const imageSpinners = document.querySelectorAll('.image-spinner > div');
+
     function changeImage() {
-        console.log(`changeImage current index ${currentIndex}`);
-        imageUrl = `url('${imagesFolder}${imageNames[currentIndex]}')`;
-        modal.style.backgroundImage = `${imageUrl}, radial-gradient(circle at 0%, #008545 20%, #008545 55%, #fff 55%)`;
-        currentIndex = (currentIndex + 1) % imageNames.length;
-        timeoutID  = setTimeout(changeImage, 3500); 
-        console.log(`changeBIMMAGE current index ${currentIndex}`);
-        if(testStarted) {
-            console.log('testStarted: ', testStarted);
+        imageUrl = `url('${imagesFolder}${imageNames[currentIndex++]}')`;
+        imageSpinners[currentImage].style.backgroundImage = imageUrl;
+        imageSpinners[currentImage].classList.add('visible');
+
+        timeoutID = setTimeout(() => {imageSpinners[currentImage].classList.remove('visible');}, 3000);
+        
+        currentIndex = currentIndex % imageNames.length;
+        currentImage = (currentImage + 1) % 2;
+
+        if (testStarted) {
+            imageSpinners.forEach(spinner => spinner.classList.remove('visible'));
+            clearTimeout(timeoutID);
             return;
+        } else {
+            console.log('testStarted: ', testStarted);
         }
     }
-    if(testStarted) {
-        console.log('testStarted: ', testStarted);
-        return;
-    }
-    changeImage();
-}
 
+    if(!testStarted) setInterval(changeImage, 3000);
+}
