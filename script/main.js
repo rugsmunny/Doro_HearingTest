74391ff10ec4b913c7fcd9c8503a61a7c2c38336
rugsmunny <karim.sendesni@gmail.com>
2024-04-02 12:02:59 +0200

    syncing latest updates, startpage, page 1 and 2 nearly finished

--- script/main.js
+++ script/main.js
@@ -1,30 +1,18 @@
-
-
-
 document.addEventListener('DOMContentLoaded', changeBackgroundPictures);
-/*
-function sayHi(){
-    const alarmBtn = Object.assign(document.createElement('button'), { id: 'alarm'});
-    alarmBtn.innerText = 'PUSH ME';
-    alarmBtn.onclick = playback;
-    document.querySelector('#test').textContent = "SCRIPTET FUNKAR!";
-    document.querySelector('body').append(alarmBtn);
 
-        const soundToGet = '8000_50';
-    console.log(soundToGet);
-    new Audio(`resources/sounds/testsounds/${soundToGet}.ogg`).play();
-}*/
+
 const modal = document.querySelector('.app');
-const form = document.querySelector('.title');
+const title = document.querySelector('.title');
 let testStarted = false;
 let timeoutID = null;
 function startTest(event) {
     event.preventDefault();
+    startBtn.classList.add('hide');
     testStarted = true;
     const infoSegments = document.querySelectorAll('.info');
     infoSegments.forEach(segment => segment.classList.add('hide'));
     modal.classList.add('hide');
-    form.textContent = `Testet har startat. Du kommer att höra en serie ljud. Klicka på knappen när du hör ljudet.`;
+    title.textContent = `First tell us a little about yourself`;
 }
 
 const startBtn = document.querySelector('#start-test-btn');
@@ -35,7 +23,7 @@ let imageUrl = '';
 
 function changeBackgroundPictures() {
     const imagesFolder = 'resources/images/hearing_buds/';
-    const imageNames = ['1-black-box-top.png', '2-closed-black-box-top-45-degree.png', '3-closed-black-box-front.png', '4-open-case-front-no-grip.png', '5-closer-to-tilted-case.png', '6-up-down-no-grip.png', '7-big-small-no-grip.png', '8-all-tips-no-grip.png', '9-phone-with-aqua-cover-open.png'];
+    const imageNames = ['2-closed-black-box-top-45-degree.png', '3-closed-black-box-front.png', '4-open-case-front-no-grip.png', '5-closer-to-tilted-case.png', '6-up-down-no-grip.png', '7-big-small-no-grip.png', '8-all-tips-no-grip.png', '9-phone-with-aqua-cover-open.png', '1-black-box-top.png'];
     let currentIndex = 0;
     let currentImage = 0;
     const imageSpinners = document.querySelectorAll('.image-spinner > div');
@@ -61,3 +49,18 @@ function changeBackgroundPictures() {
 
     if(!testStarted) setInterval(changeImage, 3000);
 }
+
+
+
+/*
+function sayHi(){
+    const alarmBtn = Object.assign(document.createElement('button'), { id: 'alarm'});
+    alarmBtn.innerText = 'PUSH ME';
+    alarmBtn.onclick = playback;
+    document.querySelector('#test').textContent = "SCRIPTET FUNKAR!";
+    document.querySelector('body').append(alarmBtn);
+
+        const soundToGet = '8000_50';
+    console.log(soundToGet);
+    new Audio(`resources/sounds/testsounds/${soundToGet}.ogg`).play();
+}*/
---
+++ script/page2.js
@@ -0,0 +1,39 @@
+const pageNumber = document.querySelector('#page-number-digit');
+const navigationButtons = document.querySelectorAll('.navigation-button');
+
+
+navigationButtons[0].style.display = 'none';
+navigationButtons[1].addEventListener('mouseover', showTooltip);
+
+navigationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const pageNavigation = parseInt(button.dataset.direction);
+        getPage(pageNavigation);
+    });
+});
+
+function getPage(pageNavigation) {
+    let currentPageNumber = parseInt(pageNumber.textContent);
+    currentPageNumber += pageNavigation;
+    
+    navigationButtons[0].style.display = currentPageNumber > 1 ? 'inline-block' : 'none';
+    navigationButtons[1].style.display = currentPageNumber < 4 ? 'inline-block' : 'none';
+    
+    pageNumber.textContent = currentPageNumber;
+}
+
+function showTooltip() {
+    const tooltip = document.querySelector('.tooltip');
+    tooltip.style.display = 'block';
+    button.addEventListener('mouseout', () => {
+        const tooltip = document.querySelector('.tooltip');
+        if (tooltip) {
+            tooltip.remove();
+        }
+    });
+}
+const totalPages = 4;
+const progressPercentage = (currentPageNumber / totalPages) * 100;
+
+const progressBarInner = document.querySelector('.progress-bar-inner');
+progressBarInner.style.width = progressPercentage + '%';
