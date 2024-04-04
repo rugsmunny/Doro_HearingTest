b708d77147933cb142ae42b857a602bb6d83232a
rugsmunny <karim.sendesni@gmail.com>
2024-04-04 14:23:14 +0200

    init branch commit

--- script/main.js
+++ script/main.js
@@ -9,13 +9,41 @@ function sayHi(){
     document.querySelector('body').append(alarmBtn);
 }*/
 
-function playback(event){
+function playback(event) {
     event.preventDefault();
     const soundToGet = '8000_50';
     console.log(soundToGet);
     new Audio(`resources/sounds/testsounds/${soundToGet}.ogg`).play();
 }
 
-const startBtn = document.querySelector('#start-test-btn');
+let pageNumber = 1;
+let navButtons = document.querySelectorAll('.nav-button');
+navButtons.forEach(button => button.addEventListener('click', function () {
+console.log(`pageNumber : ${pageNumber}`);
+    const slideToRemove = document.getElementById(`slide-${pageNumber}`);
+    pageNumber += parseInt(button.dataset.direction);
+    console.log(`pageNumber : ${pageNumber}`);
+    const slideToShow = document.getElementById(`slide-${pageNumber}`);
+    slideToRemove.classList.add('fade-out');
 
-startBtn.onclick = playback;
+    setTimeout(function () {
+        slideToRemove.style.display = 'none';
+        slideToShow.style.display = 'flex';
+        slideToShow.classList.add('fade-in');
+    }, 2000);
+
+
+}));
+
+const selectElement = document.getElementById("year-of-birth");
+const currentYear = new Date().getFullYear();
+let option = document.createElement("option");
+option.textContent = "";
+selectElement.appendChild(option);
+for (var year = currentYear; year >= currentYear - 120; year--) {
+    option = document.createElement("option");
+    option.value = year;
+    option.textContent = year;
+    selectElement.appendChild(option);
+}
+
