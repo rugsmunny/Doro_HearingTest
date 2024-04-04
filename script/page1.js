4a4eb578769b4f3a12715e982c19a82f8ca90db8
karim <karim.sendesni@gmail.com>
2024-04-04 03:05:27 +0200

    continues work on slide 2 and 3, html, css

--- script/page1.js
+++ script/page1.js
@@ -2,7 +2,7 @@ const pageNumber = document.querySelector('#page-number-digit');
 const navigationButtons = document.querySelectorAll('.navigation-button');
 
 
-navigationButtons[0].style.display = 'none';
+//navigationButtons[0].style.display = 'none';
 navigationButtons[1].addEventListener('mouseover', showTooltip);
 
 navigationButtons.forEach(button => {
@@ -16,8 +16,8 @@ function getPage(pageNavigation) {
     let currentPageNumber = parseInt(pageNumber.textContent);
     currentPageNumber += pageNavigation;
     
-    navigationButtons[0].style.display = currentPageNumber > 1 ? 'inline-block' : 'none';
-    navigationButtons[1].style.display = currentPageNumber < 4 ? 'inline-block' : 'none';
+/*     navigationButtons[0].style.display = currentPageNumber > 1 ? 'inline-block' : 'none';
+    navigationButtons[1].style.display = currentPageNumber < 4 ? 'inline-block' : 'none'; */
     
     pageNumber.textContent = currentPageNumber;
 }
