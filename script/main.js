b47abd3e62f25e559dd780f7cc5d120209c23de2
rugsmunny <karim.sendesni@gmail.com>
2024-04-08 09:05:20 +0200

    style changes, form input logic added

--- script/main.js
+++ script/main.js
@@ -19,7 +19,11 @@ function playback(event) {
 let pageNumber = 1;
 let navButtons = document.querySelectorAll('.nav-button');
 navButtons.forEach(button => button.addEventListener('click', function () {
-console.log(`pageNumber : ${pageNumber}`);
+    console.log(`pageNumber : ${pageNumber}`);
+
+    if (button.classList.contains('inactive')) {
+        return;
+    }
     const slideToRemove = document.getElementById(`slide-${pageNumber}`);
     pageNumber += parseInt(button.dataset.direction);
     console.log(`pageNumber : ${pageNumber}`);
@@ -28,22 +32,56 @@ console.log(`pageNumber : ${pageNumber}`);
 
     setTimeout(function () {
         slideToRemove.style.display = 'none';
+        slideToRemove.classList.remove('fade-out');
         slideToShow.style.display = 'flex';
         slideToShow.classList.add('fade-in');
     }, 2000);
+}));
+const formRequirements = [0, 0, 0];
+const dateOfBirthInput = document.getElementById('year-of-birth');
 
+dateOfBirthInput.addEventListener('input', function () {
 
+    const currentYear = new Date().getFullYear();
+    if (parseInt(this.value) > currentYear || parseInt(this.value) < currentYear - 100) {
+        alert('Please enter a valid year of birth.');
+        formRequirements[0] = 0;
+    } else {
+        formRequirements[0] = 1;
+    }
+    validateForm();
+});
+
+const genderRadioButtons = document.querySelectorAll('.input-radio');
+genderRadioButtons.forEach(radioButton => radioButton.addEventListener('click', function () {
+    if (Array.from(genderRadioButtons).some(radioButton => radioButton.checked)) {
+        formRequirements[1] = 1;
+    }
+    validateForm();
 }));
 
-const selectElement = document.getElementById("year-of-birth");
-const currentYear = new Date().getFullYear();
-let option = document.createElement("option");
-option.textContent = "";
-selectElement.appendChild(option);
-for (var year = currentYear; year >= currentYear - 120; year--) {
-    option = document.createElement("option");
-    option.value = year;
-    option.textContent = year;
-    selectElement.appendChild(option);
+const soundCheckBoxes = document.querySelectorAll('.input-checkbox');
+const otherInput = document.getElementById('other-text');
+
+function validateForm() {
+
+    soundCheckBoxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            if (checkbox.id === 'other-checkbox' && otherInput.value.length < 5) {
+                alert('Please elaborate on your answer in the text field.');
+                return;
+            }
+            formRequirements[2] = 1;
+        }
+    });
+    
+    if (formRequirements.reduce((acc, val) => acc + val) === 3) {
+        document.getElementById('form-btn').classList.remove('inactive');
+    } else {
+        document.getElementById('form-btn').classList.add('inactive');
+    }
 }
+
+
+
 
